e87eb319ea9ccb5cff8e5c50a313e3129ee12846
kingola1 <segun8427@gmail.com>
2022-02-01 13:22:59 +0100

    Estech Assesment

--- src/App.js
+++ src/App.js
@@ -1,34 +1,50 @@
-import React, { useState, useEffect } from 'react';
-
+import React, { Component } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea } from '@mui/material';
-// import './App.css';
 
-import { getGames } from './api';
+import axios from 'axios';
 
-import './components/GameLists';
+const api = axios.create({
+    baseURL: `https://adaorachi.github.io/esetech-assessment-api/game-data.json`
+  })
+  
+class App extends Component{
+    
+    constructor(){
+        super();
+        api.get('/').then (res => {
+          console.log(res.data)
+        })
+      }
 
-const App = () => {
-
-  const [Games, setGames] = useState([]);
-
-  useEffect(() => {
-    getGames()
-      .then((data) => {
-        console.log(data);
-
-        setGames(data);
-      })
-  }, []);
-
-
-
-    return (
-        <h1>App</h1>
-    );
+      render(){
+          return (
+            <Card sx={{ maxWidth: 345 }}>
+                 <CardActionArea>
+                   <CardMedia
+                     component="img"
+                     height="140"
+                     image="/static/images/cards/contemplative-reptile.jpg"
+                     alt="green iguana"
+                   />
+                   <CardContent>
+                     <Typography gutterBottom variant="h5" component="div">
+                       Lizard
+                     </Typography>
+                     <Typography variant="body2" color="text.secondary">
+                       Lizards are a widespread group of squamate reptiles, with over 6,000
+                       species, ranging across all continents except Antarctica
+                     </Typography>
+                   </CardContent>
+                 </CardActionArea>
+               </Card>
+          );
+      }
+    
 }
+
 
 export default App;
